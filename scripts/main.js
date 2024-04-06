dd2b71f829fced75dce14ab083de47713a52f085
AnneKluytmans <annekluytmans1@gmail.com>
2024-04-06 12:07:46 +0200

    Finished assignments 1 and 2

--- scripts/main.js
+++ scripts/main.js
@@ -72,3 +72,48 @@ const departments = {
 }
 
 console.log(departments);
+
+//--------------------------- Opdracht 1 -----------------------------------------
+
+// Opdracht 1a
+
+console.log("De afdeling Sales heeft", departments.sales.numberOfEmployees, "medewerkers.");
+
+// Opdracht 1b
+
+console.log("Marketing is een leuke afdeling om te werken.", departments.marketing.description);
+
+// Opdracht 1c
+
+console.log("De afdeling Customer Service heeft", departments["customer-service"].numberOfEmployees, "medewerkers.");
+
+// Opdracht 1d
+
+console.log("Sales is een uitdagende afdeling om te werken als Verkoopmanager.", departments.sales.jobs[1].description);
+
+
+//-------------------------- Opdracht 2 --------------------------------------------
+
+// Opdracht 2a
+
+const departmentChoice = prompt("Over welke afdeling wil je meer informatie? Kies uit: [marketing / sales / customer-service]");
+console.log(departmentChoice);
+
+// Opdracht 2b en 2c
+
+switch (departmentChoice) {
+    case "marketing":
+        console.log("Je koos:", departmentChoice + ".", departments.marketing.description);
+        break;
+    case "sales":
+        console.log("Je koos:", departmentChoice + ".", departments.sales.description);
+        break;
+    case "customer-service":
+        console.log("Je koos:", departmentChoice + ".", departments["customer-service"].description);
+        break;
+    default:
+        console.error("Ongeldige keuze. Probeer het opnieuw door de pagina te verversen.");
+}
+
+//------------------------ Opdracht 3 ------------------------------------------------
+
